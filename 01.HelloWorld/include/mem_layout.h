a71bec59943c1edf853b12bb821c7e86129e97e8
dendrobiumz <page19920610@gmail.com>
2025-02-02 14:41:08 +0800

    [Add] add kernel memory layout header

---
+++ 01.HelloWorld/include/mem_layout.h
@@ -0,0 +1,9 @@
+#ifndef _MEM_LAYOUT_H
+#define _MEM_LAYOUT_H
+
+#define   UART_BASE       (0x10000000L)
+#define   CLINT           (0x2000000L)
+
+#define   CLINT_MTIME     (CLINT + 0xBFF8)
+
+#endif
