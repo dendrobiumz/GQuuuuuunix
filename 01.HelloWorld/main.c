95a7b9fe988bb638216de36970d6917d12f10961
dendrobiumz <page19920610@gmail.com>
2025-03-10 21:03:43 +0800

    [Update] remove redundant comment

--- 01.HelloWorld/main.c
+++ 01.HelloWorld/main.c
@@ -5,8 +5,6 @@
 
 void kernel_main(void) 
 {
-    //memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
-
     u64 cpu_id = r_tp();
     if (cpu_id == 0)
         printf("[CPU%d] Boot Successfully. Now it's in kernel main function.\n", cpu_id);
