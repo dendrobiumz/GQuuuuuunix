dadf29113f43fb65e5869d94d3cf41b9a58e7510
dendrobiumz <page19920610@gmail.com>
2025-05-08 23:17:48 +0800

    [Update] init time

--- 02.TimerInterrupt/main.c
+++ 02.TimerInterrupt/main.c
@@ -1,6 +1,10 @@
 #include "types.h"
 #include "console.h"
 #include "riscv.h"
+#include "fixed_point.h"
+
+
+fixed_point_t time;
 
 void kernelvec();
 
@@ -11,6 +15,8 @@ void kernel_main(void)
     if (cpu_id == 0)
         printf("[CPU-%d] Boot Successfully. Now it's in kernel main function.\n\n", cpu_id);
     
+    time.data = 0x0;
+    time.mantissa_bit = 32;
     intr_on();
     for (;;)
         asm volatile("wfi");
