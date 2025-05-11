7c36405a1ea0bac67cbfeaa46030ad849edbd848
dendrobiumz <page19920610@gmail.com>
2025-05-11 21:45:38 +0800

    [Create] implement test_set ,compare_exchange and memory barrier

---
+++ 03.Scheduler/include/atomic_ops.h
@@ -0,0 +1,25 @@
+#ifndef _ATOMIC_OPS_H
+#define _ATOMIC_OPS_H
+#include "riscv.h"
+#include "types.h"
+
+static inline void smp_mb(void)
+{ 
+    __asm__ volatile("fence rw, rw" ::: "memory");
+}
+
+int __cmpxchg(void *ptr, u64 expected, u64 desired, i64 sz);
+
+#define CMPXCHG(ptr, old, new) ({ \
+    typeof(*(ptr)) __old = (old); \
+    typeof(*(ptr)) __new = (new); \
+    __cmpxchg(ptr, old, new, sizeof(*(ptr))); \
+})
+
+int __test_set(void *ptr);
+
+#define TEST_SET(ptr) ({ \
+    __test_set(ptr, sizeof(*(ptr))); \
+})
+
+#endif
