508698988b5d5ec6b8d1b17844a6d48429f12c23
dendrobiumz <page19920610@gmail.com>
2025-06-06 22:07:46 +0800

    [Update] modify PANIC format

--- 03.Scheduler/include/console.h
+++ 03.Scheduler/include/console.h
@@ -14,7 +14,7 @@ void puts(char *s);
 
 #define PANIC(fmt, ...)                                                        \
     do {                                                                       \
-        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
+        printf("[PANIC]: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
         while (1) {}                                                           \
     } while (0)
 
