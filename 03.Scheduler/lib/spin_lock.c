b86609f34a1090129262a7c20c9187a1fd6e2cee
dendrobiumz <page19920610@gmail.com>
2025-06-06 22:46:51 +0800

    [Update] replace intr_on/intr_off as push_off/pop_off. update isLocked new type with atomic operation.

--- 03.Scheduler/lib/spin_lock.c
+++ 03.Scheduler/lib/spin_lock.c
@@ -1,15 +1,17 @@
 #include "spin_lock.h"
-#include "atomic_ops.h"
+#include "atomic.h"
 #include "sched.h"
 #include "console.h"
+#include "task.h"
 
 static void acquire_lk(struct spin_lock *lk)
 {
-    intr_off();
-    if (!lk->is_holding(lk))
-        PANIC("[PANIC] spin lock acquire");
-    while(TEST_SET(&lk->isLocked))
+    push_off();
+    if (lk->is_holding(lk))
+        PANIC("spin lock acquire is holding");
+    while (TEST_SET(&lk->isLocked) != 0)
         ;
+        
     smp_mb();
     lk->cpu = get_cur_cpu();
 }
@@ -17,24 +19,23 @@ static void acquire_lk(struct spin_lock *lk)
 static void release_lk(struct spin_lock *lk)
 {
     if (!lk->is_holding(lk))
-        PANIC("[PANIC] spin lock release");
+        PANIC("spin lock release not holding");
     
     lk->cpu = NULL;
     smp_mb();
-    lk->isLocked = 0;
-    intr_on();
+    ATOMIC_STORE(&lk->isLocked, 0);
+    pop_off();
 }
 
 static int is_holding_lk(struct spin_lock *lk)
 {
-    if (get_cur_cpu() == lk->cpu)
-        return 1;
-    return 0;
+    int r = lk->isLocked.val && get_cur_cpu() == lk->cpu;
+    return r;
 }
 
 int init_spin_lock(struct spin_lock *lk)
 {
-    lk->isLocked = 0;
+    lk->isLocked.val = 0;
     lk->cpu = NULL;
     lk->acquire = acquire_lk;
     lk->release = release_lk;
