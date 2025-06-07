f98c752fb75af1d3e1c5b9ff601c867d5aee33fc
dendrobiumz <page19920610@gmail.com>
2025-06-07 19:05:27 +0800

    [Create] create mm header and source file

---
+++ 04.VirtualMemory/include/mm.h
@@ -0,0 +1,8 @@
+#ifndef MM_H
+#define MM_H
+
+void* kalloc();
+
+int kfree(void *addr);
+
+#endif
---
+++ 04.VirtualMemory/lib/mm.c
@@ -0,0 +1,15 @@
+#include "mm.h"
+
+extern char end[];
+
+void *kalloc()
+{
+
+}
+
+int kfree(void *addr)
+{
+    int err;
+
+    return err;
+}
