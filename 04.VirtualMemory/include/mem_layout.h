bef1144ec391f57d688419ce30ee8ba34fb651cf
dendrobiumz <page19920610@gmail.com>
2025-06-07 19:21:51 +0800

    [Update] add kernel base address and physical address stop

--- 04.VirtualMemory/include/mem_layout.h
+++ 04.VirtualMemory/include/mem_layout.h
@@ -7,4 +7,7 @@
 #define   CLINT_MTIME             (CLINT_BASE + 0xBFF8)
 #define   CLINT_MTIMECMP(hartid)  (CLINT_BASE + 0x4000 + 8 * (hartid))
 
+#define   KERNBASE                 0x80000000L
+#define   PA_STOP                 (KERNBASE + 128*1024*1024)
+
 #endif
