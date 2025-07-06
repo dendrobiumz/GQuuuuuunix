86a6a8182e7723b0b6de105d9693cb823a7ba914
dendrobiumz <page19920610@gmail.com>
2025-07-06 22:30:28 +0800

    [Update] allocate task by memory allocator and some test

--- 04.VirtualMemory/main.c
+++ 04.VirtualMemory/main.c
@@ -5,10 +5,10 @@
 #include "sched.h"
 #include "task.h"
 #include "atomic.h"
+#include "mm.h"
 
 fixed_point_t time;
 
-static struct task_struct task1, task2, task3, task4, task5;
 
 extern struct task_struct gTasks[TASK_MAX_NRS];
 
@@ -23,6 +23,7 @@ void kernel_main(void)
     start.val = 0;
     w_stvec((u64)kernelvec);
     if (cpu_id == 0) {
+        kinit_pa();
         printf("GQuuuuuunix Kernel Booting ... \n\n");
         time.data = 0x0;
         time.mantissa_bit = 32;
@@ -30,27 +31,34 @@ void kernel_main(void)
         for (int i = 0; i < TASK_MAX_NRS; i++)
             init_spin_lock(&gTasks[i].lk);
 
+        struct task_struct *task1, *task2, *task3, *task4, *task5;
+        task1 = (struct task_struct*) _kalloc();
+        init_task(task1, 0);
+        addTask(task1);
         
-        init_task(&task1, 0);
-        addTask(&task1);
+        task2 = (struct task_struct*) _kalloc();
+        init_task(task2, 1);
+        addTask(task2);
+        
+        task3 = (struct task_struct*) _kalloc();
+        init_task(task3, 2);
+        addTask(task3);
 
-        init_task(&task2, 1);
-        addTask(&task2);
-
-        init_task(&task3, 2);
-        addTask(&task3);
-
-        init_task(&task4, 3);
-        addTask(&task4);
-
-        init_task(&task5, 4);
-        addTask(&task5);
+        task4 = (struct task_struct*) _kalloc();
+        init_task(task4, 3);
+        addTask(task4);
+        
+        task5 = (struct task_struct*) _kalloc();
+        init_task(task5, 4);
+        addTask(task5);
 
         printf("GQuuuuuunix Boot Successfully ... \n\n", cpu_id);
         ATOMIC_STORE(&start, 1);
     } else {
+        void *p = page_alloc(8 * cpu_id);
         while (ATOMIC_LOAD(&start) == 0)
             ;
+        page_free(p, 8 * cpu_id);
     }
 
     scheduler();
